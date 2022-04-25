e6c997e9e3f652112132f9ebafd23a58d0a1576e
Trevor Gibby <trevor@ninthroot.com>
2022-04-24 22:42:21 -0600

    api and storage added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "tgnotesapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "tgamplifyapp": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
